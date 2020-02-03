4cdd74bacdb8d93918fda778b4b4ab194784ab67
maashkinoozhe <m.moeller@teletomtom.de>
2020-02-03 10:29:26 +0100

    update help output

--- PunchIt/PunchItClient/Program.cs
+++ PunchIt/PunchItClient/Program.cs
@@ -48,13 +48,14 @@ namespace PunchItClient
             UserInterface.Print("This is \"Punch It!\"");
             UserInterface.Print("");
 
-            UserInterface.Print("\t-h\t--help\t\t Print this help.");
+            UserInterface.Print("\t-h\t--help\t\t\t Print this help.");
 
             UserInterface.Print("\t-e\t--export\t\t Run in export mode, export saved records into csv formatted file.");
 
-            UserInterface.Print("\t-s\t--start-work\t\t Start working on a package in the currently active project if the day has started(no RecordEntries created so far)");
-            UserInterface.Print("\t\tusage\t -s <PackageKey>)");
-            UserInterface.Print("\t\tusage\t --start-work <PackageKey>)");
+            UserInterface.Print("\t-s\t--start-work\t\t Start working on a package in the currently)");
+            UserInterface.Print("\t\t\t\t\t active project if the day has started(no RecordEntries created so far)");
+            UserInterface.Print("\t\tusage\t -s <PackageKey>");
+            UserInterface.Print("\t\tusage\t --start-work <PackageKey>");
 
             UserInterface.Print("");
         }
