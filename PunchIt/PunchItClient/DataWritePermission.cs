12885f132a113d41c48a7811847998f927e3dbf6
maashkinoozhe <m.moeller@teletomtom.de>
2020-02-03 10:31:21 +0100

    extracted class into seperate file

---
+++ PunchIt/PunchItClient/DataWritePermission.cs
@@ -0,0 +1,16 @@
+﻿namespace PunchItClient
+{
+    internal class DataWritePermission
+    {
+        public DataWritePermission(bool updateState, bool updateProject, bool updateRecord)
+        {
+            UpdateState = updateState;
+            UpdateProject = updateProject;
+            UpdateRecord = updateRecord;
+        }
+
+        public bool UpdateState { get; set; }
+        public bool UpdateProject  {get;set;}
+        public bool UpdateRecord { get; set; }
+    }
+}
--- PunchIt/PunchItClient/Program.cs
+++ PunchIt/PunchItClient/Program.cs
@@ -498,19 +498,5 @@ namespace PunchItClient
             var currentRecord = GetCurrentRecord(state) ?? CreateNewRecord(state);
             return project;
         }
-
-        private class DataWritePermission
-        {
-            public DataWritePermission(bool updateState, bool updateProject, bool updateRecord)
-            {
-                UpdateState = updateState;
-                UpdateProject = updateProject;
-                UpdateRecord = updateRecord;
-            }
-
-            public bool UpdateState { get; set; }
-            public bool UpdateProject  {get;set;}
-            public bool UpdateRecord { get; set; }
-        }
     }
 }
